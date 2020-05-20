4d1f5628f9cfa22f8b011f87b3de89a0821f37e2
yjz17377133 <949076471@qq.com>
2020-05-21 02:32:01 +0800

    update voice_controler

---
+++ demo/vel_pkg/src/voice_move.cpp
@@ -0,0 +1,141 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+
+#define CMD_STOP        0
+#define CMD_FORWARD     1
+#define CMD_BACKWARD    2  
+#define CMD_LEFT        3
+#define CMD_RIGHT       4   
+
+#define CMD_DURATION    30
+
+static ros::Publisher vel_pub;
+static ros::Publisher spk_pub;
+static int nCmd = CMD_STOP;
+static int nCount = 0;
+
+void KeywordCB(const std_msgs::String::ConstPtr & msg)
+{
+    //ROS_WARN("[KeywordCB] - %s",msg->data.c_str());
+    geometry_msgs::Twist vel_cmd;
+    vel_cmd.linear.x = 0;
+    vel_cmd.linear.y = 0;
+    vel_cmd.linear.z = 0;
+    vel_cmd.angular.x = 0;
+    vel_cmd.angular.y = 0;
+    vel_cmd.angular.z = 0;
+
+    bool bCmd = false;
+    int nFindIndex = 0;
+    nFindIndex = msg->data.find("forward");
+    if( nFindIndex >= 0 )
+    {
+        //ROS_WARN("[KeywordCB] - move x");
+        bCmd = true;
+        nCmd = CMD_FORWARD;
+    }
+    nFindIndex = msg->data.find("backward");
+    if( nFindIndex >= 0 )
+    {
+        //ROS_WARN("[KeywordCB] - move -x");
+        bCmd = true;
+        nCmd = CMD_BACKWARD;
+    }
+    nFindIndex = msg->data.find("left");
+    if( nFindIndex >= 0 )
+    {
+        //ROS_WARN("[KeywordCB] - move y");
+        bCmd = true;
+        nCmd = CMD_LEFT;
+    }
+
+    nFindIndex = msg->data.find("right");
+    if( nFindIndex >= 0 )
+    {
+        //ROS_WARN("[KeywordCB] - move -y");
+        bCmd = true;
+        nCmd = CMD_RIGHT;
+    }
+
+    nFindIndex = msg->data.find("stop");
+    if( nFindIndex >= 0 )
+    {
+        //ROS_WARN("[KeywordCB] - stop");
+        bCmd = true;
+        nCmd = CMD_STOP;
+    }
+
+
+    std_msgs::String strSpeak;
+    if(bCmd == true)
+    {
+        nCount = CMD_DURATION;
+        strSpeak.data = "好的";
+        spk_pub.publish(strSpeak);
+    }
+    else
+    {
+        //strSpeak = *msg;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "voice_move");
+
+    ros::NodeHandle n;
+    ros::Subscriber sub_sr = n.subscribe("/xfyun/iat", 10, KeywordCB);
+    spk_pub = n.advertise<std_msgs::String>("/xfyun/tts", 20);
+    vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+
+    ros::Rate r(10);
+    while(ros::ok())
+    {
+        geometry_msgs::Twist vel_cmd;
+        if(nCount > 0)
+        {
+            nCount --;
+            if(nCmd == CMD_FORWARD)
+            {
+                vel_cmd.linear.x = 0.1;
+                //ROS_WARN("forward");
+            }
+            if(nCmd == CMD_BACKWARD)
+            {
+                vel_cmd.linear.x = -0.1;
+                //ROS_WARN("backward");
+            }
+            if(nCmd == CMD_LEFT)
+            {
+                vel_cmd.angular.z = 0.1;
+                //ROS_WARN("left");
+            }
+            if(nCmd == CMD_RIGHT)
+            {
+                vel_cmd.angular.z = -0.1;
+                //ROS_WARN("right");
+            }
+            if(nCmd == CMD_STOP)
+            {
+                vel_cmd.linear.x = 0;
+                vel_cmd.linear.y = 0;
+                vel_cmd.angular.z = 0;
+                //ROS_WARN("stop");
+            }
+        }
+        else
+        {
+            nCmd = CMD_STOP;
+            vel_cmd.linear.x = 0;
+            vel_cmd.linear.y = 0;
+            vel_cmd.angular.z = 0;
+            //ROS_WARN("stop");
+        }
+        vel_pub.publish(vel_cmd);
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    return 0;
+}
